e5ce2da7fa2bba98819138646084aac0145ecf25
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-19 12:29:24 +0000

    add an addtional test case to addtion function

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -2,9 +2,12 @@ const addition = require("../calc");
 
 describe("Calculator", () => {
     describe("Addition function", () => {
-        test("should return 42 for 20 +22", () => {
+        test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
-        })
+        });
+        test("should return 32 for 20 + 12", () => {
+            expect(addition(20, 12)).toBe(32);
+        });
 
     })
     describe("Subtraction function", () => {
