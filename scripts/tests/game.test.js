005be2f68b43bbcf411f34a2470d0d58a3151a53
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-19 14:11:22 +0000

    add testcase to check content of game.choices and add content to game.choices

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -16,14 +16,17 @@ beforeAll(() => {
 describe("score key exists", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
-    })
+    });
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
-    })
+    });
     test("playerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
-    })
+    });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
-    })
+    });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 })
