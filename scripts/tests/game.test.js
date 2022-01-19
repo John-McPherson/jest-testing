a75fb07dd08f845541bb558254e50d1850c5e4bf
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-19 13:19:06 +0000

    add test case to see if score object exists in game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,5 +2,15 @@
  * @jest-environment jsdom
  */
 beforeAll(() => {
-    let
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe("score key exists", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    })
 })
