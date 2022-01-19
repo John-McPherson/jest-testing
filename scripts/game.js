7337c3bc8a3f37cd0126b53363865edf525cf67c
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-19 17:04:12 +0000

    add testcase to check for turnNumber in game object and add turnNumber to game object

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
+    turnNumber: 0,
 }
 
 function newGame() {
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -31,6 +31,9 @@ describe("score key exists", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
     test("choices contains correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
