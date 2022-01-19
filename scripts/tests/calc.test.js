acc5b799bba6b84274ca2b54ee3291273c7cb91a
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-19 13:11:16 +0000

    comment out code for calc.test.js

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,25 +1,25 @@
-/**
- * @jest-environment jsdom
- */
-const addition = require("../calc");
+// /**
+//  * @jest-environment jsdom
+//  */
+// const addition = require("../calc");
 
-describe("Calculator", () => {
-    describe("Addition function", () => {
-        test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42);
-        });
-        test("should return 32 for 20 + 12", () => {
-            expect(addition(20, 12)).toBe(32);
-        });
+// describe("Calculator", () => {
+//     describe("Addition function", () => {
+//         test("should return 42 for 20 + 22", () => {
+//             expect(addition(20, 22)).toBe(42);
+//         });
+//         test("should return 32 for 20 + 12", () => {
+//             expect(addition(20, 12)).toBe(32);
+//         });
 
-    })
-    describe("Subtraction function", () => {
+//     })
+//     describe("Subtraction function", () => {
 
-    })
-    describe("Multiply function", () => {
+//     })
+//     describe("Multiply function", () => {
 
-    })
-    describe("Division function", () => {
+//     })
+//     describe("Division function", () => {
 
-    })
-})
+//     })
+// })
